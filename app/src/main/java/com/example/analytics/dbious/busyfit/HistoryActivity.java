59973eb758279f2cdd1116a761a692d4f8ceb4e1
Charles Thompson <charles.thompson4748@gmail.com>
2015-11-24 12:30:16 -0600

    ListViews are back!
    list views in all the activities using the basic android adapter

--- app/src/main/java/com/example/analytics/dbious/busyfit/HistoryActivity.java
+++ app/src/main/java/com/example/analytics/dbious/busyfit/HistoryActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 /**
  * Created by charlie on 11/24/2015.
@@ -13,6 +16,16 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // Simple array with a list of my favorite TV shows
+        String[] favoriteTVShows = {"South Park", "Better Off Ted",
+                "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
+                "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
+
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, favoriteTVShows);
+
+        ListView theListView = (ListView) findViewById(R.id.workout_list);
+
+        theListView.setAdapter(theAdapter);
     }
 
 //    @Override
--- app/src/main/java/com/example/analytics/dbious/busyfit/Today.java
+++ app/src/main/java/com/example/analytics/dbious/busyfit/Today.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,23 +22,30 @@ public class Today extends AppCompatActivity {
                 "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
                 "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
 
-        ArrayList<String> workoutList = new ArrayList<>();
-        workoutList.addAll(Arrays.asList(favoriteTVShows));
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, favoriteTVShows);
+
+        ListView theListView = (ListView) findViewById(R.id.workout_list);
+
+        theListView.setAdapter(theAdapter);
+
+        //theListView.setOnItemClickListener(new AdapterView);
+        //ArrayList<String> workoutList = new ArrayList<>();
+        //workoutList.addAll(Arrays.asList(favoriteTVShows));
 
         //MyAdapter workoutListAdapter = (MyAdapter) new ArrayAdapter<>(this, R.layout.workout_list,
         //        R.id.workout_list_item, workoutList);
 
-        ListView theListView = (ListView) findViewById(R.id.workout_list);
+        //ListView theListView = (ListView) findViewById(R.id.workout_list);
 
         //theListView.setAdapter(workoutListAdapter);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_today, menu);
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_today, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
