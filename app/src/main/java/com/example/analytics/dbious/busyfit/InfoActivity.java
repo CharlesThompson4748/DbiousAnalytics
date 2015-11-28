957dcb1670ce9b78d0b68ca185cff1bee08ee387
Charles Thompson <charles.thompson4748@gmail.com>
2015-11-28 11:26:31 -0600

    running app

--- app/src/main/java/com/example/analytics/dbious/busyfit/InfoActivity.java
+++ app/src/main/java/com/example/analytics/dbious/busyfit/InfoActivity.java
@@ -27,20 +27,4 @@ public class InfoActivity extends AppCompatActivity {
 
         theListView.setAdapter(theAdapter);
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_today, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        return id == R.id.action_settings || super.onOptionsItemSelected(item);
-//    }
 }
