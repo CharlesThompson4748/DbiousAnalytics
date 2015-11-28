139d1932812af224525e5f1abb49dc7111a52c53
Charles Thompson <charles.thompson4748@gmail.com>
2015-11-28 11:46:58 -0600

    info workout llist

--- app/src/main/java/com/example/analytics/dbious/busyfit/InfoActivity.java
+++ app/src/main/java/com/example/analytics/dbious/busyfit/InfoActivity.java
@@ -8,20 +8,26 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
 /**
  * Created by charlie on 11/24/2015.
  */
 public class InfoActivity extends AppCompatActivity {
+    private DatabaseHandler dbHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_activity);
-        // Simple array with a list of my favorite TV shows
-        String[] favoriteTVShows = {"Futurama", "Better Off Ted",
-                "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
-                "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
 
-        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, favoriteTVShows);
+        List workoutList = dbHandler.getAllWorkoutScheduleEntries();
+        // Simple array with a list of my favorite TV shows
+//        String[] favoriteTVShows = {"Futurama", "Better Off Ted",
+//                "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
+//                "Breaking Bad", "The 400", "Alphas", "Life on Mars"};
+
+        ListAdapter theAdapter = new ArrayAdapter<List>(this, android.R.layout.simple_list_item_1, workoutList);
 
         ListView theListView = (ListView) findViewById(R.id.workout_list);
 
